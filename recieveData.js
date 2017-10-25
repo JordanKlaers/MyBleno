7de2910cc98b0ccd6437168b52f42b8517b05cf8
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-24 22:06:26 -0600

    sending a number as a string then converting to number type

--- recieveData.js
+++ recieveData.js
@@ -1,8 +1,7 @@
 var handleTheData = (data) =>{
   console.log("Recieve data file :3 ----------------");
-  console.log(typeof data);
+
   var number = Number(data)
-  console.log(typeof number);
   console.log(number);
 }
 
