c0fc261d0a0e6faf8b09bfc473ac2d22cb75fbe1
jordanklaers <jordan.klaers@gmail.com>
2018-04-16 21:11:31 -0600

    started advertising when connection s otps

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ var recieveData = require("./recieveData.js");
 console.log("util: ", util);
 
 var WriteOnlyCharacteristic = function() {
-  WriteOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff5',
-    properties: ['write', 'writeWithoutResponse']
-  });
+  	WriteOnlyCharacteristic.super_.call(this, {
+    	uuid: 'fffffffffffffffffffffffffffffff5',
+    	properties: ['write', 'writeWithoutResponse']
+  	});
 };
 
 
 util.inherits(WriteOnlyCharacteristic, BlenoCharacteristic);
 
 WriteOnlyCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
-  var converted = data.toString('base64');
-  var b = new Buffer(converted, 'base64');
-  var result = b.toString();
+  	var converted = data.toString('base64');
+  	var b = new Buffer(converted, 'base64');
+  	var result = b.toString();
 
-  recieveData.handleTheData(result);
-  callback(this.RESULT_SUCCESS);
+  	recieveData.handleTheData(result);
+  	callback(this.RESULT_SUCCESS);
 };
 
 
 function SampleService() {
-  SampleService.super_.call(this, {
-    uuid: '00112233-4455-6677-8899-aabbccddeeff',
-    characteristics: [
-      new WriteOnlyCharacteristic(),
-    ]
-  });
+  	SampleService.super_.call(this, {
+	    uuid: '00112233-4455-6677-8899-aabbccddeeff',
+	    characteristics: [
+      	new WriteOnlyCharacteristic(),
+    	]
+  	});
 }
 
 util.inherits(SampleService, BlenoPrimaryService);
@@ -55,30 +55,31 @@ bleno.on('accept', function(clientAddress) {
 });
 
 bleno.on('disconnect', function(clientAddress) {
-  console.log('_My bleno_: on -> disconnect, client: ' + clientAddress);
+  	console.log('_My bleno_: on -> disconnect, client: ' + clientAddress);
 });
 
 bleno.on('rssiUpdate', function(rssi) {
-  console.log('_My bleno_: on -> rssiUpdate: ' + rssi);
+  	console.log('_My bleno_: on -> rssiUpdate: ' + rssi);
 });
 //////////////////////////////////////
 
 bleno.on('mtuChange', function(mtu) {
-  console.log('_My bleno_: on -> mtuChange: ' + mtu);
+  	console.log('_My bleno_: on -> mtuChange: ' + mtu);
 });
 
 bleno.on('advertisingStart', function(error) {
-  console.log('_My bleno_: on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+  	console.log('_My bleno_: on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
 
-  if (!error) {
-    bleno.setServices([
-      new SampleService()
-    ]);
-  }
+  	if (!error) {
+    	bleno.setServices([
+      		new SampleService()
+    	]);
+  	}
 });
 
 bleno.on('advertisingStop', function() {
-  console.log('_My bleno_: on -> advertisingStop');
+	bleno.startAdvertising('raspberrypi', ['fffffffffffffffffffffffffffffff0']);
+  	console.log('_My bleno_: on -> advertisingStop');
 });
 
 bleno.on('servicesSet', function(error) {
