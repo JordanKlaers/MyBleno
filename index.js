00b6887a64720a300beb9c4435ac191339481a19
jordanklaers <jordan.klaers@gmail.com>
2018-04-21 20:00:23 -0600

    testing with bluetooth

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var firmata = require('firmata');
 var board = new five.Board({
 	io: new raspi(),
   });
-// var board = new firmata.Board("/dev/mmcblk0p2", function(){
-	board.on('ready', function(){
+var board = new firmata.Board("/dev/mmcblk0p2", function(){
+	// board.on('ready', function(){
 console.log('made board');
     strip = new pixel.Strip({
         pin: 6, // this is still supported as a shorthand
